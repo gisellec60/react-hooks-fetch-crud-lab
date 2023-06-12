1e4999403abeab240e5e12c287ed7682a5ab5a82
Giselle <gisellec60@gmail.com>
2023-06-12 16:53:19 -0400

    still not working

--- src/components/App.js
+++ src/components/App.js
@@ -7,78 +7,31 @@ function App() {
   const [page, setPage] = useState("List");
   const [questions, setQuestions] = useState([])
   
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-     .then((res) => res.json())
-     .then((questions) => setQuestions(questions))
-  },[])
-
   const handleAddQuestion = ((newData) => {
     setQuestions([...questions,newData])
   }) 
 
-  const handleDeleteQuestion = ((deletedId) => {
-     const upDatedQuestions = questions.filter((question) => question.id !==deletedId)
-     setQuestions(upDatedQuestions)
- })
-
- const handleUpdatedCorrectIndex = ((newIndex) => {
-    const updatedQuestions = questions.map((question) => {
-      if (question.id === newIndex.id) {
-          return newIndex
-      }else{
-          return question
-      }  
-    })
-    setQuestions(updatedQuestions)
- })
-
-  const handleDeleteClick = ((id) => {
-    fetch(`http://localhost:4000/questions/${id}`,{
-      method: "DELETE"
-     })
-     .then (res=> res.json())
-     .then (() => handleDeleteQuestion(id))
-  })
- 
-  const handleUpdateAnswer = ((id,e) => {
-    console.log(id, e.target.value, )
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type" : "application/json"
-      },
-      body:JSON.stringify
-      (
-        {"correctIndex": e.target.value} 
-      )
-    })
-    .then((res) => res.json()) 
-    .then((newIndex) => handleUpdatedCorrectIndex(newIndex)) 
-  }) 
-
-const displayQuestions = questions.map((question) => (
-      <div key={question.id}>
-        <li> {question.prompt}
-          <button onClick={() => handleDeleteClick(question.id)}>Delete Question</button>
-          <label>
-           Correct Answer 
-          <select name="correctIndex" onChange={(e) => handleUpdateAnswer(question.id,e)} value={question.correctIndex}>
-            {question.answers.map((answer, index) => (
-              <option key={index} value={index}>{answer}</option>
-            ))}
-          </select>
-          </label>
-        </li>
-       </div> 
-  ))
- 
-  return (
+    return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm onAddQuestion={handleAddQuestion} /> : <QuestionList displayQuestions={displayQuestions} 
+      {page === "Form" ? <QuestionForm onAddQuestion={handleAddQuestion} /> : <QuestionList questions={questions} setQuestions={setQuestions}
        />}
     </main>
   );
 }
 export default App;
+// const displayQuestions = questions.map((question) => (
+//   <div key={question.id}>
+//     <li> {question.prompt}
+//       <button onClick={() => handleDeleteClick(question.id)}>Delete Question</button>
+//       <label>
+//        Correct Answer 
+//       <select name="correctIndex" onChange={(e) => handleUpdateAnswer(question.id,e)} value={question.correctIndex}>
+//         {question.answers.map((answer, index) => (
+//           <option key={index} value={index}>{answer}</option>
+//         ))}
+//       </select>
+//       </label>
+//     </li>
+//    </div> 
+// ))
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,7 +1,68 @@
 import React, {useState, useEffect} from "react";
 
-function QuestionList({displayQuestions}) {
+function QuestionList({questions, setQuestions}) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+     .then((res) => res.json())
+     .then((questions) => setQuestions(questions))
+  },[])
   
+  const handleDeleteQuestion = ((deletedId) => {
+    const upDatedQuestions = questions.filter((question) => question.id !==deletedId)
+    setQuestions(upDatedQuestions)
+})
+
+const handleUpdatedCorrectIndex = ((newIndex) => {
+   const updatedQuestions = questions.map((question) => {
+     if (question.id === newIndex.id) {
+         return newIndex
+     }else{
+         return question
+     }  
+   })
+   setQuestions(updatedQuestions)
+})
+
+  const handleDeleteClick = ((id) => {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+     })
+     .then (res=> res.json())
+     .then (() => handleDeleteQuestion(id))
+  })
+ 
+  const handleUpdateAnswer = ((id,e) => {
+    console.log(id, e.target.value, )
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body:JSON.stringify
+      (
+        {"correctIndex": e.target.value} 
+      )
+    })
+    .then((res) => res.json()) 
+    .then((newIndex) => handleUpdatedCorrectIndex(newIndex)) 
+  }) 
+
+  const displayQuestions = questions.map((question) => (
+    <div key={question.id}>
+      <li> {question.prompt}
+        <button onClick={() => handleDeleteClick(question.id)}>Delete Question</button>
+        <label>
+         Correct Answer 
+        <select name="correctIndex" onChange={(e) => handleUpdateAnswer(question.id,e)} value={question.correctIndex}>
+          {question.answers.map((answer, index) => (
+            <option key={index} value={index}>{answer}</option>
+          ))}
+        </select>
+        </label>
+      </li>
+     </div> 
+))
   return (
     <section>
       <h1>Quiz Questions</h1>
